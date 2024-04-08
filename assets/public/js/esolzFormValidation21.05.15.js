e913adafdaf51b4f03a1f73679e5b865faa798dd
surajit-bappa <166256787+surajit-bappa@users.noreply.github.com>
2024-04-08 11:06:22 -0500

    Add files via upload

---
+++ assets/public/js/esolzFormValidation21.05.15.js
@@ -0,0 +1,221 @@
+(function($){
+    $.fn.extend({
+        
+        preOption:{
+            errorTextColor: 'red',
+            errorFieldColor: 'red'
+        },
+        ErrerAdd: function(selecter,errorShow,message){
+            selecter.next('.ErrorMag').remove();
+            selecter.css('border-color','');
+            if (message == '') {
+                selecter.css('border-color',this.preOption.errorFieldColor);
+            }
+            else{
+                selecter.css('border-color',this.preOption.errorFieldColor);
+                if (errorShow == '') {
+                    selecter.after('<div class="ErrorMag" style="color:'+this.preOption.errorTextColor+';">'+message+'</div>');
+                }
+                else{
+                    $(errorShow).html('<div class="ErrorMag" style="color:'+this.preOption.errorTextColor+';">'+message+'</div>');
+                }
+            }
+        },
+        ErrerFree: function(selecter,errorShow){
+            selecter.next('.ErrorMag').remove();
+            $(errorShow).html('');
+            selecter.css('border-color','');
+        },
+        commonCheck: function(options){
+
+            var defaults = {
+                errorArea: '',
+                errorMessage1: 'This field is required'
+            };
+            
+            options_v = $.extend(defaults,options);
+            
+            msg = options_v.errorMessage1;
+            if((typeof options == "undefined") || (typeof options == "")){
+                msg = options_v.errorMessage1;
+            }
+            else if ((typeof options == "string")) {
+                msg = options;
+            }
+            if ($(this).length == 1) {
+                var ThisValue = $.trim($(this).val());
+                if (ThisValue == '') {
+                    this.ErrerAdd($(this),options_v.errorArea,msg);
+                    return false;
+                }
+                else{
+                   this.ErrerFree($(this),options_v.errorArea);
+                   return true;
+                }
+            }
+            else{
+                return false;
+            }
+        },
+        validateEmail: function(options){
+        
+            var defaults = {
+                errorArea: '',
+                errorMessage1: 'Email is required',
+                errorMessage2: 'Please enter a valid e-mail address'
+            };
+            
+            options_v = $.extend(defaults,options);
+
+            msg1 = options_v.errorMessage1;
+
+            var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+            if ($(this).length == 1) {
+                var ThisValue = $.trim($(this).val());
+                if (ThisValue == '') {
+                    this.ErrerAdd($(this),options_v.errorArea,msg1);
+                    return false;
+                }
+                else if (!pattern.test(ThisValue)) {
+                    
+                    msg2 = options_v.errorMessage2;
+                    if((typeof options == "undefined") || (typeof options == "")){
+                        msg2 = options_v.errorMessage2;
+                    }
+                    else if ((typeof options == "string")) {
+                        msg2 = options;
+                    }
+                    
+                    this.ErrerAdd($(this),options_v.errorArea,msg2);
+                    return false;
+                }
+                else{
+                   this.ErrerFree($(this),options_v.errorArea);
+                   return true;
+                }
+                
+            }
+            else{
+                return false;
+            }
+        },
+        CkConfirmPassword: function(options){
+            if ($(this).length == 1) {
+                $(this).css('border-color','');
+                var defaults = {
+                    passwordField: '',
+                    errorArea: '',
+                    errorMessage1: 'This field is required',
+                    errorMessage2: 'Password and confirm password not match'
+                };
+                options_v = $.extend(defaults,options);
+               
+                ThisValue = $.trim($(this).val());
+                if (ThisValue == '') {
+                    this.ErrerAdd($(this),options_v.errorArea,options_v.errorMessage1);
+                    return false;
+                }
+                else if ((options_v.passwordField == '')) {
+                    this.ErrerAdd($(this),options_v.errorArea,'<b>Please provide the password field</b>');
+                    return false;
+                }
+                else if ($(options_v.passwordField).length == 0) {
+                    this.ErrerAdd($(this),options_v.errorArea,'<b>Please provide the password field</b>');
+                    return false;
+                }
+                else if (ThisValue != $.trim($(options_v.passwordField).val())) {
+                    this.ErrerAdd($(this),options_v.errorArea,options_v.errorMessage2);
+                    return false;
+                }
+                else{
+                   this.ErrerFree($(this),options_v.errorArea);
+                   return true;
+                }
+            }
+            else{
+                return false;
+            }
+        },
+        checkFileType: function(options){
+            
+            if ($(this).length == 1) {
+                var defaults = {
+                    allowedExtensions: [], //like ['jpg', 'jpeg']
+                    errorArea: '',
+                    errorMessage1: 'This field is required',
+                    errorMessage2: 'Wrong file type'
+                };
+                options_v = $.extend(defaults,options);
+                ThisValue = $.trim($(this).val());
+                if (ThisValue == '') {
+                    this.ErrerAdd($(this),options_v.errorArea,options_v.errorMessage1);
+                    return false;
+                }
+                else{
+                    if (options_v.allowedExtensions.length > 0) {
+                        ThisValueLower = ThisValue.toLowerCase();
+                        extension = ThisValueLower.substring(ThisValueLower.lastIndexOf('.') + 1);
+                        if ($.inArray(extension, options_v.allowedExtensions) == -1) {
+                            this.ErrerAdd($(this),options_v.errorArea,options_v.errorMessage2);
+                            return false;
+                        }
+                        else{
+                            this.ErrerFree($(this),options_v.errorArea);
+                            return true;
+                        }
+                    }
+                    else{
+                        this.ErrerFree($(this),options_v.errorArea);
+                        return true;
+                    }
+                }
+            }
+            else{
+                return false;
+            }
+        },
+        checkRadioOrCheck: function(options){
+            
+            //Demo $("input[name=s]:checked").checkRadioOrCheck();
+            //  if  name is not array (input[name=s]) 
+            //  if name is an array (input[name^=s]) 
+            var msg = '';
+            var defaults = {
+                errorArea : '',
+                errorMessage1: 'This field is required'
+            };
+            options_v = $.extend(defaults,options);
+            
+            $(options_v.errorArea).html('');
+            
+            if ((options === undefined) || (options == '')) {
+                msg = '';
+            }
+            else if (typeof options == 'string') {
+                msg = options;
+            }
+            else if (typeof options == 'object') {
+                msg = options_v.errorMessage1;
+            }
+            
+            selectorName = $(this).selector;
+
+            if ($(selectorName+':checked').length <= 0) {
+                    if (options_v.errorArea !='') {
+                        $(options_v.errorArea).html('<span style="color:'+this.preOption.errorTextColor+'">'+options_v.errorMessage1+'</span>');
+                    }
+                    else{
+                        this.ErrerAdd($(this),msg);
+                    }
+                    return false;
+            }
+            else{
+                   this.ErrerFree($(this));
+                   return true;
+            }
+            
+        }
+        
+    })
+})(jQuery);
+    
---
+++ assets/public/js/jquery.dOb.js
@@ -0,0 +1,91 @@
+/**
+ * My dOb Plugin
+ *
+ * @Date    20131119
+ * @Author  ShawnWu
+ * @Version     release v1.6.20161125
+ * @License     under the MIT License
+ **/
+jQuery.fn.dOb = function(opts) {
+    opts = jQuery.extend({
+        dOb_Y: '#dOb_Y', // element year
+        dOb_M: '#dOb_M', // element month
+        dOb_D: '#dOb_D', // element day
+        from_Y: 2000, // from year
+        to_Y: 2023, // to year
+        isPadding: true // is padding zero
+    }, opts);
+
+    var myBirthDate = function() {
+        var _Y = $(opts.dOb_Y),
+            _M = $(opts.dOb_M),
+            _D = $(opts.dOb_D),
+            def_Y = _Y.val(),
+            def_M = _M.val(),
+            def_D = _D.val(),
+            option = "<option></option>";
+
+        _year();
+        _month();
+        _day(1);
+        _Y.on('change', _day);
+        _M.on('change', _day);
+
+        function _year() {
+            var i = null;
+            if (opts.from_Y > opts.to_Y) {
+                for (i = opts.from_Y; i >= opts.to_Y; i--) {
+                    if (i == def_Y) {
+                        _Y.append($(option).attr({ "value": i, "selected": true }).text(i));
+                        _Y.find('option:first').remove();
+                    } else _Y.append($(option).attr("value", i).text(i));
+                }
+            } else {
+                for (i = opts.from_Y; i <= opts.to_Y; i++) {
+                    if (i == def_Y) {
+                        _Y.append($(option).attr({ "value": i, "selected": true }).text(i));
+                        _Y.find('option:first').remove();
+                    } else _Y.append($(option).attr("value", i).text(i));
+                }
+            }
+        }
+
+        function _month() {
+            for (var i = 1; i <= 12; i++) {
+                var MM = opts.isPadding ? ('0' + i).slice(-2) : ('' + i);
+                if (i == def_M) {
+                    _M.append($(option).attr({ "value": i, "selected": true }).text(MM));
+                    _M.find('option:first').remove();
+                } else _M.append($(option).attr("value", i).text(MM));
+            }
+        }
+
+        function _day(dd) {
+            var def_YD = $.isNumeric(_Y.val()) ? _Y.val() : 1911,
+                def_MD = $.isNumeric(_M.val()) ? _M.val() : 1,
+                dateTime = new Date(def_YD, def_MD, 0), // get last date of month
+                D = dd == 1 ? 1 : 0;
+
+            if (D && !$.isNumeric(def_D))
+                _D.append($(option).attr({ "value": def_D, "selected": true }).text(def_D));
+
+            _D.children().each(function() {
+                // remove days of month
+                if ($(this).val() > dateTime.getDate()) $(this).remove();
+            });
+
+            for (var i = 1; i <= dateTime.getDate(); i++) {
+                // insert days of month
+                if (!_D.children("option[value='" + i + "']").length) {
+                    var DD = opts.isPadding ? ('0' + i).slice(-2) : ('' + i);
+                    if (i == def_D) {
+                        _D.append($(option).attr({ "value": i, "selected": true }).text(DD));
+                        _D.find('option:first').remove();
+                    } else _D.append($(option).attr("value", i).text(DD));
+                }
+            }
+        }
+    };
+
+    myBirthDate();
+};
